4b5dac1d2024171aeb423afe545140379837c6a6
Edmilson <edmilson@infoway-pi.com.br>
2017-05-30 13:56:05 -0300

    rotações simples e duplas

--- src/main/java/operar/Rotor.java
+++ src/main/java/operar/Rotor.java
@@ -3,6 +3,42 @@ package operar;
 import arvore.No;
 
 public class Rotor {
+
+	public static No duplaEsquerda(No raiz) {
+		raiz.dir = simplesDireita(raiz.dir);
+		raiz = simplesEsquerda(raiz);
+		
+		return raiz;
+	}
+
+	public static No duplaDireita(No raiz) {
+		raiz.esq = simplesEsquerda(raiz.esq);
+		raiz = simplesDireita(raiz);
+		
+		return raiz;
+	}
+	
+	public static No simplesEsquerda(No raiz) {
+		No novaRaiz, tmp;
+		
+		novaRaiz = raiz.dir;
+		tmp = novaRaiz.esq;
+		novaRaiz.esq = raiz;
+		raiz.dir = tmp;
+		
+		return novaRaiz;
+	}
+	
+	public static No simplesDireita(No raiz) {
+		No novaRaiz, tmp;
+		
+		novaRaiz = raiz.esq;
+		tmp = novaRaiz.dir;
+		novaRaiz.dir = raiz;
+		raiz.esq = tmp;
+		
+		return novaRaiz;
+	}
 	
 	public static No rotacionar(No desbalanceado, No raiz) {
 		if (desbalanceado == null) return raiz;
